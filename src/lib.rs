6539b351c5e60666923a37ee2b556bd1a4d15f62
Connor George <58805178+Si1veR123@users.noreply.github.com>
2025-09-23 21:12:48 +0000

    this should work

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,14 @@ pub enum DirectoryNode {
 
 impl DirectoryNode {
     pub fn try_from_path<P: AsRef<Path>>(path: P) -> Option<Self> {
-        std::fs::create_dir_all(&path).ok()?;
+        let path = path.as_ref();
+        
+        if !path.exists() {
+            std::fs::create_dir_all(&path).ok()?;
+        }
+
         let path = canonicalize(path.as_ref()).ok()?;
+
         if path.is_dir() {
             let mut children = Vec::new();
             if let Ok(entries) = std::fs::read_dir(&path) {
